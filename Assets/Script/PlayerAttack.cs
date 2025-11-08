1ccc85f2ae360217eb2aa00bb77b27dd8c3fe0f3
bulangya818 <139843735+bulangya818@users.noreply.github.com>
2025-11-08 14:31:44 +0800

    攻击

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -7,13 +7,19 @@ public class PlayerAttack : MonoBehaviour
     public Animator animator;
 
     public GameObject RangeTrigger;
+    
+    private bool _attacking = false;
+
+    public Player3 player3;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !_attacking)
         {
             animator.SetTrigger("Attack");
+            _attacking = true;
+            player3.enabled = false;
         }
     }
 
@@ -25,5 +31,11 @@ public class PlayerAttack : MonoBehaviour
     public void EndAttack()
     {
         RangeTrigger.SetActive(false);
+    }
+
+    public void AttackAnimationFinished()
+    {
+        _attacking = false;
+        player3.enabled = true;
     }
 }
